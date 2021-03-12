12dcfed90868a81c085260619b25465968b9bfeb
pdywilson <pdywilson@gmail.com>
2021-03-12 20:48:09 +0000

    update

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -22,7 +22,7 @@ val urls = Map(
   "dublinrents_4rooms"->"https://www.daft.ie/property-for-rent/dublin-city?numBeds_from=4&sort=publishDateDesc&from=%s&pageSize=20"
 )
 
-val r = (0 to 100).map(x => x*20) //ToDo: change to 2000 later (100)
+val r = (0 to 2).map(x => x*20) //ToDo: change to 2000 later (100)
 
 val urls2 = urls.map {case (k, v) => k -> r.map(x => v.format(x))}
 
@@ -31,38 +31,56 @@ val urls2 = urls.map {case (k, v) => k -> r.map(x => v.format(x))}
 //println(urls2.getClass())
 
 
-val url = s"https://www.daft.ie/property-for-rent/dublin-city/apartments?numBeds_to=2&numBeds_from=1&sort=publishDateDesc&from=%s&pageSize=20".format(0)
-
-//println(urls2("dublinrents")(0))
 
 val numberPattern: Regex = "€.{1,9}per month".r
 val browser = JsoupBrowser()
 
+val url = urls2("dublinrents")(0)
+
+
+// val output = one_url >> elementList("span")
+// val output2 = output.map(_ >> allText("span"))
+// val output3 = output2.map(elt => numberPattern.findAllMatchIn(elt)).flatten
+// val output4 = output3.map(d => d.toString.dropRight(10).drop(1).replace(",","").toInt)
+// println(output4)
+
+
+// def processOneUrl(url: String) = Future { 
+//   numberPattern
+//   .findAllMatchIn(browser.get(url) >> allText)
+//   .toList
+//   .map(d => d.toString.dropRight(10).drop(1).replace(",","").toInt)
+//  }
 def processOneUrl(url: String) = Future { 
-  numberPattern
-  .findAllMatchIn(browser.get(url) >> allText)
-  .toList
-  .map(d => d.toString.dropRight(10).drop(1).replace(",","").toInt)
+  val one_url = browser.get(url)
+  val output4 = one_url
+  //val output4 = (one_url >> allText).take(0)
+  // val output = one_url >> elementList("span")
+  // val output2 = output.map(_ >> allText("span"))
+  // val output3 = output2.map(elt => numberPattern.findAllMatchIn(elt)).flatten
+  // val output4 = output3.map(d => d.toString.dropRight(10).drop(1).replace(",","").toInt)
+  output4
  }
+
 
 
 // val one = processOneUrl(url)
 // val actualResult = Await.result(one, 10 seconds)
-// print(actualResult)
+// print(actualResult.take(0))
 
-
-// val dos = urls2("dublinrents").map(url => processOneUrl(url))
-// val actualResult = dos.map(one => Await.result(one, 100 seconds))
-// print(actualResult.flatten)
-
+// Step: Map
 val urls3 = urls2.map {
   case (table, urls) => table -> urls.map(url => processOneUrl(url))
 }
+// Step: Reduce
 val urls4 = urls3.map {
-  case (table, futures) => table -> futures.map(one => Await.result(one, 30 seconds)).flatten
+  case (table, futures) => table -> futures.map(one => Await.result(one, 10 seconds))//.flatten
 }
-print(urls4)
+val print_urls = urls4.map {case (table, lists) => table -> lists.length}
+print(print_urls)
+print("done")
 
+// takes 43 sec at 100 with new implementation
 
 // val result = one.onComplete{
 //   case Success(result) => print(result)//result.toList.map(d => d.toString.dropRight(10).drop(1).replace(",","").toInt)).flatten
